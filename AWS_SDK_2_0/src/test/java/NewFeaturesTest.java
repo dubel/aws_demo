95677b00428f576b93f1d6698300a8a22065baab
Michael Dubel <michal.dubel@gmail.com>
2017-11-26 21:14:32 +0100

    Added further SDK 2.0 examples

--- AWS_SDK_2_0/src/test/java/NewFeaturesTest.java
+++ AWS_SDK_2_0/src/test/java/NewFeaturesTest.java
@@ -1,7 +1,13 @@
 import org.junit.Test;
 import software.amazon.awssdk.core.auth.ProfileCredentialsProvider;
 import software.amazon.awssdk.core.regions.Region;
+import software.amazon.awssdk.services.dynamodb.DynamoDBAsyncClient;
 import software.amazon.awssdk.services.dynamodb.DynamoDBClient;
+import software.amazon.awssdk.services.dynamodb.model.ListTablesRequest;
+import software.amazon.awssdk.services.dynamodb.model.ListTablesResponse;
+
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
 /**
  * Created by mike on 26.11.17.
@@ -19,9 +25,43 @@ public class NewFeaturesTest {
         DynamoDBClient client = DynamoDBClient.builder()
                 .region(Region.US_EAST_1)
                 .credentialsProvider(ProfileCredentialsProvider.builder()
-                        .profileName("my-profile")
+                        .profileName("Michal_Dubel")
                         .build())
                 .build();
+
+        ListTablesResponse response = client.listTables(ListTablesRequest.builder()
+                .limit(5)
+                .build());
+        response.tableNames().forEach(System.out::println);
+
+    }
+
+    @Test
+    public void nonBlocking() throws Exception {
+
+        // Creates a default async client with credentials and regions loaded from the environment
+        DynamoDBAsyncClient client = DynamoDBAsyncClient.create();
+        CompletableFuture<ListTablesResponse> response = client.listTables(ListTablesRequest.builder()
+                .limit(5)
+                .build());
+        // Map the response to another CompletableFuture containing just the table names
+        CompletableFuture<List<String>> tableNames = response.thenApply(ListTablesResponse::tableNames);
+        // When future is complete (either successfully or in error) handle the response
+        tableNames.whenComplete((tables, err) -> {
+            if (tables != null) {
+                tables.forEach(System.out::println);
+            } else {
+                // Handle error
+                err.printStackTrace();
+            }
+        });
+
+    }
+
+
+    @Test
+    public void nonBlockingStreaming() throws Exception {
+        
 
     }
 }
